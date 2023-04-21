d80a5c9ce4f184b23d6eb6b9d550f6c24d8e69bf
Muhammed Bilal <bilalmuhammed363@gmail.com>
2023-04-22 02:13:15 +0530

    images added,searchbox

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:shoe_rack_ecommerce/core/colors.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,30 +6,49 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        // appBar:   Icon(Icons.notifications),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-                color: colorgray,
-                borderRadius: BorderRadius.circular(20)),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.search),
-                ),
-                SizedBox(
-                    width: 300,
-                    child: TextField(
-                      decoration: InputDecoration.collapsed(hintText: 'Search'),
-                    ))
-              ],
+        // appBar: PreferredSize(
+        //   preferredSize: size,
+          // child: Image.asset('asset/images/Group 14logo vertical 1.svg')),
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: colorgray, borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.search),
+                  ),
+                  SizedBox(
+                      width: 300,
+                      child: TextField(
+                        decoration:
+                            InputDecoration.collapsed(hintText: 'Search'),
+                      ))
+                ],
+              ),
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Image.asset(
+                'asset/images/Group 21.png',
+                fit: BoxFit.contain,
+              ),
+              // child: Image.network('https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.vecteezy.com%2Farte-vectorial%2F8564775-banner-de-zapatos-deportivos-para-sitio-web-con-boton-ui-design-vector-ilustracion&psig=AOvVaw3bvoR6sdVGt_JOFa3Ifk-0&ust=1682194941338000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMC5jtHmu_4CFQAAAAAdAAAAABAE'),
+              width: size.width,
+              height: 200,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
+            ),
+          )
+        ]),
       ),
     );
   }
